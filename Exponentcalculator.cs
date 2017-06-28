e2b4c21cc0aa6e78c3ce653a3148de3c8204df60
TinStarfish <Waterstrevor68@yahoo.com>
2017-06-27 18:29:58 -0600

    Added Methods

--- Exponentcalculator.cs
+++ Exponentcalculator.cs
@@ -4,5 +4,43 @@ public class Exponentcalculator
 {
 	public Exponentcalculator()
 	{
+
+
+
 	}
+
+    public void main ()
+    {
+
+
+
+    }
+
+    public int userNumericalInput()
+    {
+
+
+    }
+
+    public int userNameInput()
+    {
+
+
+
+    }
+
+    public void returnName()
+    {
+
+
+    }
+
+    public int calculate()
+    {
+
+
+
+    }
+
+
 }
